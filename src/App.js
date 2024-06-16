3cb36761918db3096973532d3ac200e2de396593
rahulmehandiratta <rahulmehandiratta@scizers.in>
2024-06-16 21:36:35 +0530

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import Edit from "./pages/edit";
+import Add from "./pages/add";
 
-function App() {
+const App= ()=> {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="App">
 
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route exact path="/edit/:id" element={<Edit />} />
+              <Route exact path="/add" element={<Add />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+  )
+}
 export default App;
